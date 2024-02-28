6b4b80494c5a27cce7870a3ebb64475682240b56
jonathanalleman <jonathanalleman@gmail.com>
2024-02-27 20:55:51 -0600

    Module upload

---
+++ calculator.js
@@ -0,0 +1,56 @@
+let firstNumber;
+let secondNumber;
+let operation;
+
+let resultElem = document.getElementById('result')
+
+function clickNumber(num) {
+    let stringNum= new String (num);
+    if (!firstNumber){
+        firstNumber = stringNum;
+        resultElem.innerText = firstNumber;
+    }
+    else if (!operation) {
+        firstNumber+= stringNum;
+        resultElem.innerText = firstNumber;
+        
+    }
+    else if (!secondNumber){
+        secondNumber = stringNum;
+        resultElem.innerText = secondNumber;
+    
+    }
+    else{
+        secondNumber += stringNum;
+        resultElem.innerText = secondNumber;
+    }
+}
+
+function clickOperator(op){
+    operation = op;
+
+}
+
+function showResult(){
+    let first = parseInt(firstNumber)
+    let second = parseInt(secondNumber)
+    let calulation= 0;
+
+
+switch(operation) {
+  case '+': calculation = first + second; break;
+  case '-': calculation = first - second; break;
+  case '/': calculation = first / second; break;
+  case '*': calculation = first * second; break;
+}
+
+clearResult()
+firstNumber = new String(calulation)
+resultElem.innerText = calculation;
+}
+
+function clearResult(){
+  firstNumber = undefined;
+  secondNumber = undefined;
+  operation = undefined
+}
